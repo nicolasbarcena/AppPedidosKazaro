722c06cc21ae3607ae8f9c4f0df389a13e6b927c
nicolasbarcena <nicolas.barcena@kazaro.com.ar>
2025-08-20 15:59:48 -0300

    sw

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll(["/", "/index.html"]);
-    })
-  );
-});
+   self.addEventListener('install', (event) => {
+       event.waitUntil(
+           caches.open('my-cache').then((cache) => {
+               return cache.addAll([
+                   // Lista de archivos que deseas almacenar en caché
+                   '/',
+                   '/index.html',
+                   '/style.css',
+                   '/script.js',
+                   // No incluyas el HTML si no deseas que se almacene en caché
+               ]);
+           })
+       );
+   });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+   self.addEventListener('fetch', (event) => {
+       event.respondWith(
+           caches.match(event.request).then((response) => {
+               return response || fetch(event.request);
+           })
+       );
+   });
+   
